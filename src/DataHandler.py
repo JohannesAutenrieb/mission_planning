84dd6c0416dd123de70e49817955592f6e933747
Natalia <strawa.natalia@gmail.com>
2019-01-31 17:39:11 +0000

    Changed arguments in compute_distance method.

--- src/DataHandler.py
+++ src/DataHandler.py
@@ -12,12 +12,15 @@
 # NOTES:
 # - Data structure for GNC commands
 #       - friendID
-#       - commandType (TAKEOFF, LAND, WAYPOINT, PRELEASE) ---> Path planning location to be decided after meeting with dr Shin)
+#       - commandType (TAKEOFF, LAND, WAYPOINT, PRELEASE, REPAINT, ABORTMISSION) ---> Path planning location to be decided after meeting with dr Shin)
 #       - commandValue (float64):
 #           * TAKEOFF - height in m
 #           * LAND - -1
 #           * WAYPOINT - GPS corrrdinates [posX, posY, posZ]
 #           * RELSEASE - -1
+#           * REPAINT - -1
+#           * ABORTMISSION - -1
+#           * GOIDLE - 0
 #       - timestamp
 # ---------------------------------------------------------------------------------------
 from math import sqrt
@@ -36,8 +39,6 @@ class DataHandler:
         self.fooId = []
         self.fooPos = []
         self.fooTimestamp = []
-        # Other data:
-        self.distance = []
 
     def get_friendly_data(self, friendlyPackage):
         # Remove old data from lists
@@ -66,17 +67,27 @@ class DataHandler:
         self.fooTimestamp.extend(fooPackage[2])
 
     # Compute distance between foos and enemies
-    def compute_distance(self):
-        # Remove old data
-        self.distance.clear()
+    def compute_distance(self, availableFriends, unhandledEnemies):
+        # Initialize distance list
+        distance = []
+        agentDistance = []
 
         # Compute new data
-        for friend in range(len(self.friendlyId)):
-            for foo in range(len(self.fooId)):
+        for friend in availableFriends:
+
+            # Find index for the friendly id
+            friendlyIndex = self.friendlyId.index(friend)
+            agentDistance.clear()
+
+            for foo in unhandledEnemies:
+
+                fooIndex = self.fooId.index(foo)
                 # Euclidean norm p=2
-                self.distance[friend][foo] = sqrt((self.friendlyPos[friend][0] - self.fooPos[foo][0])**2 +\
-                                                       (self.friendlyPos[friend][1] - self.fooPos[foo][1])**2 +\
-                                                       (self.friendlyPos[friend][2] - self.fooPos[foo][2])**2)
+                agentDistance.append(sqrt((self.friendlyPos[friendlyIndex][0] - self.fooPos[fooIndex][0])**2 +\
+                                                       (self.friendlyPos[friendlyIndex][1] - self.fooPos[fooIndex][1])**2 +\
+                                                       (self.friendlyPos[friendlyIndex][2] - self.fooPos[fooIndex][2])**2))
+            distance.append(agentDistance)
+        return distance
 
     def get_agents_availability(self):
         # Create empty list of agents with certain status
@@ -91,7 +102,7 @@ class DataHandler:
             elif self.friendlyStatus[friend] == 'EMERGENCY':
                 agentsInEmergencyMode.append(self.friendlyId[friend])
             else:
-                agentsWithoutTask.append(self.friendlyId[friend])
+                agentsWithTask.append(self.friendlyId[friend])
 
         return agentsWithTask, agentsWithoutTask, agentsInEmergencyMode
 
@@ -99,9 +110,9 @@ class DataHandler:
     def create_task_message(self, publisher, message, Id, cmdType, cmdValue, time):
         # -----------------------------------------------------------------------------
         # Message structure
-        # Id: Id of agents which is task receiver
-        # cmdType: List of tasks
-        # cmdValue: List of command values (index must coincide with cmdType index)
+        # Id: Id of agent which is task receiver
+        # cmdType: Task name
+        # cmdValue: Task value
         # time: current mission time
         # -----------------------------------------------------------------------------
 
