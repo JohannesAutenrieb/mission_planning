5fd90a1639a8a50143a19abf4d1a4842350cc112
JohannesAutenrieb <autenrieb@hotmail.com>
2019-02-05 19:11:52 +0100

    Added Task Class to Project

---
+++ src/Task.py
@@ -0,0 +1,35 @@
+import time
+
+class Task():
+    
+    # Define attributes
+    timestamp = None
+    agentIdx = None
+    taskType = None
+    wayPointLocation = None
+    taskDeadline = None
+    # Defined States in the Mission State Machine
+    def __init__(self,agentIdx,taskType,wayPointLocation):
+        
+        self.timestamp = time.time()
+        self.agentIdx = None
+        self.taskType = None
+        self.wayPointLocation = None
+        self.taskDeadline = None
+    
+    def get_timestamp(self):
+        print('Test Output timestamp :', self.timestamp)
+
+    def get_agentIdx(self):
+        print('Test Output agentIdx :', self.agentIdx)
+
+    def get_taskType(self):
+        print('Test Output taskType :', self.taskType)
+
+    def get_wayPointLocation(self):
+        print('Test Output wayPointLocation :', self.wayPointLocation)
+
+    def get_taskDeadline(self):
+        print('Test Output taskDeadline :', self.taskDeadline)
+    
+
